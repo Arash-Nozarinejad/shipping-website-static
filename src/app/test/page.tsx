c81f35701e0a1a91c16268bf0995431e965418b7
Arash Nozarinejad <nozarinejad.arash@gmail.com>
2024-11-18 16:52:21 +0400

    updated navbarLogo.tsx to have a default class. changed navbarLogo css file and added the logo to test page for testing

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,4 +1,16 @@
+import NavbarLogo from "@/components/navbar/NavbarLogo";
+import navbarLogoData from "@/../public/navbarLogoData.json";
+import styles from "@/styles/NavbarLogo.module.css";
+
 export default function TestPage() {
-    return <h1>Test Page</h1>;
-  }
-  
+  return (
+    <div>
+      <h1>Test Page</h1>
+      <NavbarLogo
+        imageUrl={navbarLogoData.imageUrl}
+        homeUrl={navbarLogoData.homeUrl}
+        logoClass={styles.logoClass}
+      />
+    </div>
+  )
+}
--- src/components/navbar/NavbarLogo.tsx
+++ src/components/navbar/NavbarLogo.tsx
@@ -2,11 +2,22 @@ import { NavbarLogoPropType } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import styles from "@/styles/NavbarLogo.module.css";
 
-const NavbarLogo: React.FC<NavbarLogoPropType> = ({ imageUrl, homeUrl, logoClass, width = 100, height = 100 }) => {
+const NavbarLogo: React.FC<NavbarLogoPropType> = ({ 
+    imageUrl, 
+    homeUrl, 
+    logoClass = styles.logoClass, 
+    width = 100, 
+    height = 100 }) => {
     return (
         <Link href={homeUrl}>
-            <Image src={imageUrl} className={logoClass} alt="Logo" width={width} height={height} />
+            <Image 
+            src={imageUrl} 
+            className={logoClass} 
+            alt="Logo" 
+            width={width} 
+            height={height} />
         </Link>
     );
 };
