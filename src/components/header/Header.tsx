e4e231f207353e9c1147aee47622b430e515ceb2
Arash Nozarinejad <nozarinejad.arash@gmail.com>
2024-11-18 14:32:04 +0400

    typo fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import { HeaderPropType } from "@/types";
 import React from "react";
-import NavbarLogo from "../navbar/navbarLogo";
-import NavbarLinks from "../navbar/navbarLinks";
+import NavbarLogo from "../navbar/NavbarLogo";
+import NavbarLinks from "../navbar/NavbarLinks";
 
 const Header: React.FC<HeaderPropType> = ({headerClass, navLogoProp, navLinksProp}) => {
     return (
