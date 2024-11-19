45eccbe16f2973fe1fa90ecce465d000a56522c9
Arash Nozarinejad <nozarinejad.arash@gmail.com>
2024-11-19 14:11:00 +0400

    restarted everything and retrying from scratch

--- src/types/HeaderTypes.ts
+++
@@ -1,7 +0,0 @@
-import { NavbarLogoPropType, NavbarPropType } from "./NavbarTypes";
-
-export type HeaderPropType = {
-    headerClass?: string;
-    navLogoProp: NavbarLogoPropType;
-    navLinksProp: NavbarPropType;
-};
