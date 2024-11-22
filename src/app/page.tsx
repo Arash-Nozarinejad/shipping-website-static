530fe3312b207842ecc6e548a11575fcce7d5699
Arash Nozarinejad <nozarinejad.arash@gmail.com>
2024-11-22 14:27:19 +0400

    Reset everything to test creation time from scratch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import Header from "@/components/header/Header";
-import Hero from "@/components/hero/Hero";
-
 export default function Home() {
   return (
     <div>
-      <Header />
-      <Hero />
+      Hello and welcome
     </div>
   );
 }
