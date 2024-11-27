b94406d4a0e0ca402dec90bac63554568743d117
Arash Nozarinejad <nozarinejad.arash@gmail.com>
2024-11-27 14:26:41 +0400

    Top card css styling for medium size screens added

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -7,59 +7,98 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Hero = () => {
-    // state to manage input value
-    const [inputValue, setInputValue] = useState("");
-    const router = useRouter();
+  // state to manage input value
+  const [inputValue, setInputValue] = useState("");
+  const router = useRouter();
 
-    // Function to handle form submission
-    const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault(); // To prevent page refresh
-        if (inputValue.trim() !== ""){
-            router.push(`/shipping-tracking/tracking?query=${encodeURIComponent(inputValue)}`);
-        } else {
-            alert("please enter a value before submitting.");
-        }
-    };
-    return (      
-        <div className={styles.heroWrapperSM}>
-            {/** Top Card */}
-            <div className={styles.topCardSM}>
-                <form onSubmit={handleSubmit} className={styles.topCardFormSM}>
-                    <input 
-                        type="text"
-                        placeholder="Enter bill of lading"
-                        value={inputValue}
-                        onChange={(e) => setInputValue(e.target.value)}
-                        className={styles.topCardFormTextInputSM}
-                    />
-                    <button 
-                        type="submit"
-                        className={styles.topCardFormTextButtonSM}>Track Shipment</button>
-                </form>
-            </div>
-            {/** Bottom Cards */}
-            <div className={styles.bottomCardsWrapperSM}>
-                {/** Card 1 */}
-                <div className={styles.bottomCardSM}>
-                    <Image src="/icon.svg" alt="icon" width={50} height={50} className={styles.bottomCardImageSM} />
-                    <p className={styles.bottomCardParagraphSM}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <Link href="" className={styles.bottomCardLinkSM}>Learn more</Link>
-                </div>
-                {/** Card 2 */}
-                <div className={styles.bottomCardSM}>
-                    <Image src="/icon.svg" alt="icon" width={50} height={50} className={styles.bottomCardImageSM} />
-                    <p className={styles.bottomCardParagraphSM}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <Link href="" className={styles.bottomCardLinkSM}>Learn more</Link>
-                </div>
-                {/** Card 3 */}
-                <div className={styles.bottomCardSM}>
-                    <Image src="/icon.svg" alt="icon" width={50} height={50} className={styles.bottomCardImageSM} />
-                    <p className={styles.bottomCardParagraphSM}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <Link href="" className={styles.bottomCardLinkSM}>Learn more</Link>
-                </div>
-            </div>
+  // Function to handle form submission
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault(); // To prevent page refresh
+    if (inputValue.trim() !== "") {
+      router.push(
+        `/shipping-tracking/tracking?query=${encodeURIComponent(inputValue)}`
+      );
+    } else {
+      alert("please enter a value before submitting.");
+    }
+  };
+
+  return (
+    <div className={styles.heroWrapper}>
+      {/** Top Card */}
+      <div className={styles.topCard}>
+        <form onSubmit={handleSubmit} className={styles.topCardForm}>
+          <input
+            type="text"
+            placeholder="Enter bill of lading"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            className={styles.topCardFormTextInput}
+          />
+          <button
+            type="submit"
+            className={styles.topCardFormTextButton}
+          >
+            Track Shipment
+          </button>
+        </form>
+      </div>
+      {/** Bottom Cards */}
+      <div className={styles.bottomCardsWrapper}>
+        {/** Card 1 */}
+        <div className={styles.bottomCard}>
+          <Image
+            src="/icon.svg"
+            alt="icon"
+            width={50}
+            height={50}
+            className={styles.bottomCardImage}
+          />
+          <p className={styles.bottomCardParagraph}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <Link href="" className={styles.bottomCardLink}>
+            Learn more
+          </Link>
         </div>
-    )
-}
+        {/** Card 2 */}
+        <div className={styles.bottomCard}>
+          <Image
+            src="/icon.svg"
+            alt="icon"
+            width={50}
+            height={50}
+            className={styles.bottomCardImage}
+          />
+          <p className={styles.bottomCardParagraph}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <Link href="" className={styles.bottomCardLink}>
+            Learn more
+          </Link>
+        </div>
+        {/** Card 3 */}
+        <div className={styles.bottomCard}>
+          <Image
+            src="/icon.svg"
+            alt="icon"
+            width={50}
+            height={50}
+            className={styles.bottomCardImage}
+          />
+          <p className={styles.bottomCardParagraph}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <Link href="" className={styles.bottomCardLink}>
+            Learn more
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
