f8a81c960b95e70cd2c5a1c766bcdff52f9e42f2
Arash Nozarinejad <nozarinejad.arash@gmail.com>
2024-11-27 16:40:36 +0400

    removed header at frustration to redo later

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-import Header from "@/components/header/Header";
-import Hero from "@/components/hero/Hero";
+import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
   return (
--- src/components/header/header.utils.ts
+++
@@ -1,6 +0,0 @@
-import { HeaderType } from "./header.types";
-import headerData from "./header.data.json";
-
-export const getHeaderData = (): HeaderType => {
-    return headerData as HeaderType;
-}
